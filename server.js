4ea55a58f5dcf276744b06965b4f4b45b64424d7
Noel O'Connor <noel.oconnor@gmail.com>
2018-01-09 12:23:22 +0000

    updated port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
-const port = process.argv[2] || 9000;
+const port = process.argv[2] || 8080;
 
 http.createServer(function (req, res) {
   console.log(`${req.method} ${req.url}`);
