d7e8f67a7f41c54c46f51ce7178f880c72e97f3b
Mat Allen <mallen@redhat.com>
2018-06-25 14:30:27 -0500

    trying to disable keycloak to deploy on ose

--- src/main/java/com/redhat/gps/pathfinder/config/SecurityKeycloakConfig.java
+++ src/main/java/com/redhat/gps/pathfinder/config/SecurityKeycloakConfig.java
@@ -22,10 +22,11 @@ package com.redhat.gps.pathfinder.config;
  * #L%
  */
 
+
 import com.redhat.gps.pathfinder.security.AuthoritiesConstants;
 import io.github.jhipster.config.JHipsterProperties;
-import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
-import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+//import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+//import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
@@ -43,73 +44,75 @@ import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CorsFilter;
 import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
 
-@KeycloakConfiguration
+//@KeycloakConfiguration
 //@Import(SecurityProblemSupport.class)
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-public class SecurityKeycloakConfig extends KeycloakWebSecurityConfigurerAdapter {
-    private final CorsFilter corsFilter;
-
-//    private final SecurityProblemSupport problemSupport;
-
-    private final JHipsterProperties jHipsterProperties;
-
-    public SecurityKeycloakConfig(AuthenticationManagerBuilder authenticationManagerBuilder,
-                                  JHipsterProperties jHipsterProperties, CorsFilter corsFilter) {//}, SecurityProblemSupport problemSupport) {
-//        this.authenticationManagerBuilder = authenticationManagerBuilder;
-//        this.userDetailsService = userDetailsService;
-        this.jHipsterProperties = jHipsterProperties;
-//        this.rememberMeServices = rememberMeServices;
-        this.corsFilter = corsFilter;
-//        this.problemSupport = problemSupport;
-    }
-
-
-    /**
-     * Registers the KeycloakAuthenticationProvider with the authentication manager.
-     */
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(keycloakAuthenticationProvider());
-    }
-
-    /**
-     * Defines the session authentication strategy.
-     */
-    @Bean
-    @Override
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new NullAuthenticatedSessionStrategy();
-//        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring()
-            .antMatchers(HttpMethod.OPTIONS, "/**")
-            .antMatchers(HttpMethod.GET, "/**")
-            .antMatchers("/app/**/*.{js,html}")
-            .antMatchers("/i18n/**")
-            .antMatchers("/content/**")
-            .antMatchers("/swagger-ui/index.html")
-            .antMatchers("/test/**");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-        http
-            .csrf().disable()
-            .addFilterBefore(corsFilter, CsrfFilter.class)
-//            .exceptionHandling()
-//            .authenticationEntryPoint(problemSupport)
-//            .accessDeniedHandler(problemSupport)
-//            .and()
-            .authorizeRequests()
-            .antMatchers("/management/health").permitAll()
-//            .antMatchers(HttpMethod.POST, "/api/pathfinder/customers/").hasRole(AuthoritiesConstants.ADMIN)
-//            .antMatchers(HttpMethod.DELETE, "/api/pathfinder/customers/*").hasRole(AuthoritiesConstants.ADMIN)
-            .antMatchers("/api/pathfinder/customers/**/*").anonymous()// hasRole(AuthoritiesConstants.USER)
-            .anyRequest().authenticated();
-    }
+public class SecurityKeycloakConfig
+//  extends KeycloakWebSecurityConfigurerAdapter
+  {
+//    private final CorsFilter corsFilter;
+//
+////    private final SecurityProblemSupport problemSupport;
+//
+//    private final JHipsterProperties jHipsterProperties;
+//
+//    public SecurityKeycloakConfig(AuthenticationManagerBuilder authenticationManagerBuilder,
+//                                  JHipsterProperties jHipsterProperties, CorsFilter corsFilter) {//}, SecurityProblemSupport problemSupport) {
+////        this.authenticationManagerBuilder = authenticationManagerBuilder;
+////        this.userDetailsService = userDetailsService;
+//        this.jHipsterProperties = jHipsterProperties;
+////        this.rememberMeServices = rememberMeServices;
+//        this.corsFilter = corsFilter;
+////        this.problemSupport = problemSupport;
+//    }
+//
+//
+//    /**
+//     * Registers the KeycloakAuthenticationProvider with the authentication manager.
+//     */
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.authenticationProvider(keycloakAuthenticationProvider());
+//    }
+//
+//    /**
+//     * Defines the session authentication strategy.
+//     */
+//    @Bean
+//    @Override
+//    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+//        return new NullAuthenticatedSessionStrategy();
+////        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+//    }
+//
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring()
+//            .antMatchers(HttpMethod.OPTIONS, "/**")
+//            .antMatchers(HttpMethod.GET, "/**")
+//            .antMatchers("/app/**/*.{js,html}")
+//            .antMatchers("/i18n/**")
+//            .antMatchers("/content/**")
+//            .antMatchers("/swagger-ui/index.html")
+//            .antMatchers("/test/**");
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http);
+//        http
+//            .csrf().disable()
+//            .addFilterBefore(corsFilter, CsrfFilter.class)
+////            .exceptionHandling()
+////            .authenticationEntryPoint(problemSupport)
+////            .accessDeniedHandler(problemSupport)
+////            .and()
+//            .authorizeRequests()
+//            .antMatchers("/management/health").permitAll()
+////            .antMatchers(HttpMethod.POST, "/api/pathfinder/customers/").hasRole(AuthoritiesConstants.ADMIN)
+////            .antMatchers(HttpMethod.DELETE, "/api/pathfinder/customers/*").hasRole(AuthoritiesConstants.ADMIN)
+//            .antMatchers("/api/pathfinder/customers/**/*").anonymous()// hasRole(AuthoritiesConstants.USER)
+//            .anyRequest().authenticated();
+//    }
 }
